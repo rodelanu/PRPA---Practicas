4a4c492c1d3b4f5ab889fb6ad0b8d85e573d9fd1
rodelanu <117449897+rodelanu@users.noreply.github.com>
2023-03-12 18:58:14 +0100

    Add files via upload

--- practica1/opcional.py
+++ practica1/opcional.py
@@ -27,9 +27,10 @@ def get_minimum(lst):
 
 
 # Función para generar los números de un proceso y guardarlos en un buffer local
-def producer(valor, non_empty, buffer, pos, indexes, mutex):
+def producer(valor, empty, non_empty, buffer, pos, indexes, mutex):
     
     # valor        -> Value con inicialmente valor -2 
+    # emtpy
     # non_empty    -> Semaphore(0) para el nº de procesos ejecutados
     # buffer       -> Array(n) en el que se guardan los números del proceso
     # pos          -> valor entero con la posición asociada del proceso
@@ -43,6 +44,8 @@ def producer(valor, non_empty, buffer, pos, indexes, mutex):
         delay()
         
         # Bloqueamos el semáforo del proceso actual y generamos un nuevo valor
+        empty.acquire()
+        
         mutex.acquire()
         valor.value += random.randint(2,7)
         delay() 
@@ -54,10 +57,11 @@ def producer(valor, non_empty, buffer, pos, indexes, mutex):
         
 
 # Función para consumir los números generados y guardarlos en el almacén
-def merge(storage, semaphores, indexes, buffers, mutex):
+def merge(storage, empty_semaphores, non_empty_semaphores, indexes, buffers, mutex):
     
     # storage    -> Array de tamaño n*nprod para guardar los productos ordenados
-    # semaphores -> lista de los semáforos locales 'non_empty'
+    # empty_semaphores     -> lista de los semáforos locales 'empty'
+    # non_empty_semaphores -> lista de los semáforos locales 'non_empty'
     # indexes    -> Array(nprod) con las posiciones de los valores de cada proceso
     # buffers    -> lista de los buffers locales, de tipo Array(n) 
     # mutex      -> Lock() para que solamente una sección crítica se ejecute
@@ -68,11 +72,11 @@ def merge(storage, semaphores, indexes, buffers, mutex):
     
     # Comprobamos que todos los procesos han producido al menos un valor
     for i in lst:
-        semaphores[i].acquire()
+        non_empty_semaphores[i].acquire()
     
     # Liberamos los anteriores semáforos para devolverlos a sus estados originales
     for i in lst:
-        semaphores[i].release()
+        non_empty_semaphores[i].release()
     
     while True:
          
@@ -81,7 +85,7 @@ def merge(storage, semaphores, indexes, buffers, mutex):
             break
                 
         # Obtenemos el valor mínimo de los generados
-        mutex.acquire()
+        
         temp = []
         for i in range(nprod):
             if indexes[i] < n:
@@ -90,15 +94,17 @@ def merge(storage, semaphores, indexes, buffers, mutex):
                 temp.append(-1)  
                     
         minimo = get_minimum(temp[:]) 
-        print("comparando: ", temp[:])                
+        pos = temp[:].index(minimo)
+        print("comparando: ", temp[:])  
+        empty_semaphores[pos].release()              
             
         # Guardamos el valor mínimo y aumentamos la posición de guardado en el almacén  
+        mutex.acquire()
         storage[merge_index] = minimo            
         print("storage: ", storage[:])
         merge_index += 1
             
-        # Marcamos el espacio del almacén como consumido y avanzamos en la posición del buffer local
-        pos = temp[:].index(minimo)
+        # Marcamos el espacio del almacén como consumido y avanzamos en la posición del buffer local  
         buffers[pos][indexes[pos]] = -1
         indexes[pos] += 1                    
         
@@ -107,7 +113,7 @@ def merge(storage, semaphores, indexes, buffers, mutex):
             ended.acquire()
         mutex.release()
             
-        semaphores[pos].acquire()  
+        non_empty_semaphores[pos].acquire()  
         print(f"consumer {current_process().name} consumiendo {minimo}")
         delay()
 
@@ -123,7 +129,8 @@ def main():
         storage[i] = -2
         
     values = [Value('i', -2) for _ in range(nprod)]
-    semaphores = [Semaphore(0) for _ in range(nprod)]
+    empty_semaphores = [Lock() for _ in range(nprod)]
+    non_empty_semaphores = [Semaphore(0) for _ in range(nprod)]
     buffers = [Array('i', n) for _ in range(nprod)]
     
     for b in buffers:
@@ -133,12 +140,13 @@ def main():
         
     mutex = Lock()
     procesos = [Process(target = producer, name = f'p_{i}', 
-                        args=(values[i], semaphores[i], buffers[i], 
-                              i, indexes, mutex)) for i in range(nprod)]
+                        args=(values[i], empty_semaphores[i], non_empty_semaphores[i], 
+                              buffers[i], i, indexes, mutex)) for i in range(nprod)]
     
     
     merge_process = Process(target = merge, name = 'merger',
-                               args = (storage, semaphores, indexes, buffers, mutex))
+                               args = (storage, empty_semaphores, non_empty_semaphores, 
+                                       indexes, buffers, mutex))
     
     for p in procesos:
         p.start()
